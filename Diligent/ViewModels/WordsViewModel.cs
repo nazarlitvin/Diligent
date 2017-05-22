dcf3514d0ef81a97fdab31889cf0a44690d67bd0
Nazar Litvin <n.litvin41@gmail.com>
2017-05-22 15:18:03 +0300

    Refactored CalculateMistakes method using Linq.

--- Diligent/ViewModels/WordsViewModel.cs
+++ Diligent/ViewModels/WordsViewModel.cs
@@ -5,48 +5,41 @@ using System.Windows.Input;
 using Diligent.Models;
 using Diligent.Services;
 using Diligent.Views;
+using System.Linq;
 
 namespace Diligent.ViewModels
 {
-	public class NavigationMessage
-	{
-		public string TargetView { get; set; }
-		public object Parameter { get; set; }
-	}
+    public class NavigationMessage
+    {
+        public string TargetView { get; set; }
+        public object Parameter { get; set; }
+    }
 
-	public class WordsViewModel
-	{
-		public WordsViewModel()
-		{
-			var wordsServices = new WordsServices();
+    public class WordsViewModel
+    {
+        public WordsViewModel()
+        {
+            var wordsServices = new WordsServices();
 
-			WordList = wordsServices.GetWords();
+            WordList = wordsServices.GetWords();
 
-            ShowResultsCommand = new Command(() => {
+            ShowResultsCommand = new Command(() =>
+            {
                 var mistakes = CalculateMistakes();
                 var message = "Mistakes: " + mistakes;
 
                 Application.Current.MainPage = new ResultsPage();
-				MessagingCenter.Send(this, "Hi", message);
+                MessagingCenter.Send(this, "Hi", message);
             });
-		}
+        }
 
-        private int CalculateMistakes() {
-			var mistakes = 0;
-
-			foreach (var word in WordList)
-			{
-				if (word.Value?.ToLower().Trim() != word.Translate.ToLower())
-				{
-					mistakes++;
-				}
-			}
-
-            return mistakes;
+        private int CalculateMistakes()
+        {
+            return WordList.Count(w => w.Value?.ToLower().Trim() != w.Translate.ToLower());
         }
 
         public List<Word> WordList { get; set; }
 
         public ICommand ShowResultsCommand { get; }
-	}
+    }
 }
